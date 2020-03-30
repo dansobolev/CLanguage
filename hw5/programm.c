17fa10cc923dfd44a8b547f607436f563e7d116e
dansobolev <daniil.sob56@gmail.com>
2020-03-30 18:01:15 +0300

    begin of hw5

---
+++ hw5/programm.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lists {
+    char name;
+    int count;
+}lists;
+
+int main()
+{
+    char command;
+
+    while(1) {
+        printf("Create the first element of a singly linked list - 1\n");
+        printf("Add item to end of singly linked list - 2\n");
+        printf("Remove item from end of singly linked list - 3\n");
+        printf("View singly linked list - 4\n");
+        printf("Delete entire singly linked list - 5\n");
+        printf("Perform a sequential search for a given element in a singly linked list - 6\n");
+        printf("Exit the program - 0\n");
+        printf("\n");
+
+        printf("Please, enter the command number here: \n");
+        scanf("%c", &command);
+        printf("\n");
+
+        if(command == '1'){
+            printf("You chose : Create the first element of a singly linked list");
+            break;
+        }
+
+        if(command == '2'){
+            printf("You chose : Add item to end of singly linked list");
+            break;
+        }
+
+        if(command == '3'){
+            printf("You chose : Remove item from end of singly linked list");
+            break;
+        }
+
+        if(command == '4'){
+            printf("You chose : View singly linked list");
+            break;
+        }
+
+        if(command == '5'){
+            printf("You chose : Delete entire singly linked list");
+            break;
+        }
+
+        if(command == '6'){
+            printf("You chose : Perform a sequential search for a given element in a singly linked list");
+            break;
+        }
+
+        if(command == '0'){
+            printf("You chose : Exit the program");
+            break;
+        }
+
+    }
+    return 0;
+}
